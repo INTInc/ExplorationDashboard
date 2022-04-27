8141bc20a7b908749a79d2805c6eb4f0d457aee7
lukyanov <roman.lukyanov@geotoolkit.net>
2022-04-27 18:26:19 +0700

    wells data on map

--- src/components/wells-map.vue
+++ src/components/wells-map.vue
@@ -5,22 +5,28 @@
 </template>
 
 <script lang="ts">
+import { Field } from '@/Field';
+
 import { Map } from '@int/geotoolkit/map/Map';
 import { Plot } from '@int/geotoolkit/plot/Plot';
-import { Tile } from '@int/geotoolkit/map/layers/Tile';
-import { Rect } from '@int/geotoolkit/util/Rect';
+import { Tile as TileLayer } from '@int/geotoolkit/map/layers/Tile';
+import { Shape as ShapeLayer } from '@int/geotoolkit/map/layers/Shape';
 import { Options, Vue } from 'vue-class-component';
-
-const BING_KEY = 'AlBIEAbAFzZkDPtXQ2zN0MdBcIpcucCpPvSiRTeHMwpZvIizIGMM1G07NFzlW0MH';
+import { GeodeticSystem } from '@int/geotoolkit/map/GeodeticSystem';
+import { Shape } from '@int/geotoolkit/scene/shapes/Shape';
+import { AnchorType } from '@int/geotoolkit/util/AnchorType';
+import { PointerMode } from '@int/geotoolkit/controls/tools/PointerMode';
 
 @Options({
     name: 'WellsMap',
     data() {
         return {
+            map: null,
             plot: null
         }
     },
     mounted() {
+        this.createMap();
         this.createPlot();
         this.resizePlot();
         this.addResizeListener();
@@ -29,18 +35,49 @@ const BING_KEY = 'AlBIEAbAFzZkDPtXQ2zN0MdBcIpcucCpPvSiRTeHMwpZvIizIGMM1G07NFzlW0
         createPlot() {
             this.plot = new Plot({
                 canvaselement: this.$refs.canvas,
-                root: this.createMap(),
+                root: this.map,
                 autosize: true
             })
         },
         createMap() {
-            const openStreetsMapLayer = new Tile({
+            const openStreetsMapLayer = new TileLayer({
                 url: 'https://demo.int.com/osm_tiles/',
                 formatterfunction: (z: number, y: number, x: number) => z + '/' + x + '/' + y + '.png'
             });
+                
+            this.map = new Map({
+                system: GeodeticSystem.WGS84,
+                tooltip: {
+                    alignment: AnchorType.BottomCenter,
+                    offsetx: 0,
+                    offsety: 0,
+                    mode: PointerMode.Hover,
+                    autoupdate: false
+                }
+            })
+                .addLayer(openStreetsMapLayer);
 
-            return new Map()
-                .addLayer(openStreetsMapLayer)
+            this.showExplorationObjects();
+        },
+        showExplorationObjects() {
+            const field = new Field('/data/fieldB.json');
+
+            field.load().then(() => {               
+                this.map.addLayer(
+                    new ShapeLayer({
+                        alpha: 0.75,
+                        tooltip: {
+                            visible: true,
+                            formatter: (shapes: Shape[]) => shapes[0] && shapes[0].getName() || null
+                        }
+                    })
+                        .addShape(field.zone)
+                        .addShape(field.wells[0])
+                        .addShape(field.wells[1])
+                )
+                    .setZoomLevel(5)
+                    .panTo(field.position, GeodeticSystem.WGS84)
+            });
         },
         resizePlot() {
             this.plot.setSize(this.$refs.root.clientWidth, this.$refs.root.clientHeight);
