b7c5054b2cbeb4297b3c989f14e2417af6b5e866
lukyanov <roman.lukyanov@geotoolkit.net>
2022-04-28 17:08:22 +0700

    map component rewritten using composition API

--- src/components/wells-map.vue
+++ src/components/wells-map.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="wells-map" ref="root">
-        <canvas ref="canvas"/>
-    </div>
+  <div ref="container" class="wells-map">
+  <canvas ref="canvas" />
+  </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+<script setup lang="ts">
+import { onMounted, ref } from '@vue/runtime-core';
+
 import { Map } from '@int/geotoolkit/map/Map';
 import { Tile as TileLayer } from '@int/geotoolkit/map/layers/Tile';
 import { Shape as ShapeLayer } from '@int/geotoolkit/map/layers/Shape';
@@ -17,76 +18,72 @@ import { PointerMode } from '@int/geotoolkit/controls/tools/PointerMode';
 import { StretchablePlot } from '@/StrechablePlot';
 
 import { ExplorationMapAdapter } from '@/data-sources/exploration-map-adapter/ExplorationMapAdapter';
-import { ExplorationMapDrawer } from '@/drawers/ExplorationMapDrawer'; 
+import { ExplorationMapDrawer } from '@/drawers/ExplorationMapDrawer';
 
-@Options({
-    name: 'WellsMap',
-    data() {
-        return {
-            map: null,
-            plot: null
-        }
-    },
-    mounted() {
-        this.createMap();
-        this.addMapLayer();
-        this.addExplorationObjectsLayer();
-        this.createPlot();
-    },
-    methods: {
-        createMap() {        
-            this.map = new Map({
-                system: GeodeticSystem.WGS84,
-                tooltip: {
-                    alignment: AnchorType.BottomCenter,
-                    offsetx: 0,
-                    offsety: 0,
-                    mode: PointerMode.Hover,
-                    autoupdate: false
-                }
-            });
-        },
-        addMapLayer() {
-            this.map.addLayer(
-                new TileLayer({
-                    url: 'https://demo.int.com/osm_tiles/',
-                    formatterfunction: (z: number, y: number, x: number) => z + '/' + x + '/' + y + '.png'
-                })
-            );
-        },
-        addExplorationObjectsLayer() {
-            const source = new ExplorationMapAdapter();
-            const drawer = new ExplorationMapDrawer(source);
+const canvas = ref();
+const container = ref();
 
-            source.load('/data/fieldB.json')
-                .then(() => {
-                    this.map.addLayer(
-                        new ShapeLayer({
-                            alpha: 0.75,
-                            tooltip: {
-                                visible: true,
-                                formatter: (shapes: Shape[]) => shapes[0] && shapes[0].getName() || null
-                            }
-                        })
-                            .addShape(drawer.zone)
-                            .addShape(drawer.wells[0])
-                            .addShape(drawer.wells[1])
-                    )
-                        .setZoomLevel(5)
-                        .panTo(source.explorationCoordinates, GeodeticSystem.WGS84)
-                })
-        },
-        createPlot() {
-            this.plot = new StretchablePlot(this.$refs.root, this.$refs.canvas, this.map);
-        },
+function createMap() {    
+  return new Map({
+    system: GeodeticSystem.WGS84,
+    tooltip: {
+      alignment: AnchorType.BottomCenter,
+      offsetx: 0,
+      offsety: 0,
+      mode: PointerMode.Hover,
+      autoupdate: false
     }
+  });
+}
+
+function addMapLayer(map: Map) {
+  map.addLayer(
+    new TileLayer({
+      url: 'https://demo.int.com/osm_tiles/',
+      formatterfunction: (z: number, y: number, x: number) => z + '/' + x + '/' + y + '.png'
+    })
+  );
+}
+
+function addExplorationLayer(map: Map) {
+  const source = new ExplorationMapAdapter();
+  const drawer = new ExplorationMapDrawer(source);
+
+  source.load('/data/fieldB.json')
+    .then(() => {
+      map.addLayer(
+        new ShapeLayer({
+          alpha: 0.75,
+          tooltip: {
+            visible: true,
+            formatter: (shapes: Shape[]) => shapes[0] && shapes[0].getName() || null
+          }
+        })
+        .addShape(drawer.zone)
+        .addShape(drawer.wells[0])
+        .addShape(drawer.wells[1])
+      )
+      .setZoomLevel(5)
+      .panTo(source.explorationCoordinates, GeodeticSystem.WGS84)
+    })
+}
+
+function createPlot(map: Map) {
+  return new StretchablePlot(container.value, canvas.value, map);
+}
+
+onMounted(() => {
+  const map = createMap();
+
+  addMapLayer(map);
+  addExplorationLayer(map);
+  createPlot(map);
 })
-export default class WellsMap extends Vue {}
 </script>
 
 <style lang="scss" scoped>
 .wells-map {
-    padding: 0;
-    overflow: hidden;
+  padding: 0;
+  overflow: hidden;
 }
 </style>
