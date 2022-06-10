07a652ba67e52e051eac170f0e7b74052a511948
lukyanov <j4g3r113@gmail.com>
2022-06-10 13:12:08 +0700

    switched to webfonts fontawesome version again to avoid manual drawing icons on map

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import App from '@/App.vue'
 import { createApp } from 'vue'
 import { createStore, storeSymbol } from '@/store'
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
 
 createApp(App)
   .provide(storeSymbol, createStore())
   .mount('#app');
-
-library.add(fas);
-dom.watch();
